0e920a9abae24bc8b1104f3a51f597d820742139
Artem <uartemu@gmail.com>
2025-06-21 21:02:51 +0300

    Add mini app support

---
+++ hello_world/__init__.py
@@ -0,0 +1,5 @@
+"""Minimal hello_world package."""
+
+__all__ = ["main"]
+
+from .__main__ import main  # noqa: E402
---
+++ hello_world/__main__.py
@@ -0,0 +1,10 @@
+"""Run the Hello World program."""
+
+
+def main() -> None:
+    """Print hello world."""
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
